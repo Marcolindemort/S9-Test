a67a8bcef0a65af9e23b4526c6715ccd35cd2fd4
Marcolindemort <marco.mandolini15@gmail.com>
2023-12-22 16:40:29 +0100

    added new movies

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,10 @@ function App() {
 			<MyNav />
 			<Container fluid className="px-4">
 				<MyActionBar />
-				<MyGallery title={"harry%20potter"} category={"Harry Potter Saga"} />
+				<MyGallery title={"batman"} category={"Batman Movies"} />
 				<MyGallery title={"the%20lord%20of%20the%20rings"} category={"The Lord of The Rings Saga"} />
 				<MyGallery title={"star%20wars"} category={"Star Wars Saga"} />
+				<MyGallery title={"spider-man"} category={"Spider-Man Movies"} />
 				<MyFooter />
 			</Container>
 		</div>
